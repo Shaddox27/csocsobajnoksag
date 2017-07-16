79ee4806aeaa0f0997baf9f724b6a6d50befc108
Mike Bodoczky <shaddox27@yahoo.com>
2017-07-16 12:12:14 +0200

    run_tournament.js hozzáadása és összekötése a főoldallal.

---
+++ Scripts/run_tournament.js
@@ -0,0 +1,20 @@
+
+//A lejátszás gombra kattintva kiszámolja az eredményeket, és HTML-ben megjeleníti azokat.
+function runTournamentEventHandler()
+{
+ 	calculateScores();
+ 	displayresults();
+}
+
+
+function calculateScores()
+{
+ 	//TODO
+}
+
+
+function displayresults()
+{
+ 	//TODO
+}
+
